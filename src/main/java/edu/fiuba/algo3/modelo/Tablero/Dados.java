4fadc03dfa82f0d8c69f86eade9555e9b93f4db8
Leo <accruz@fi.uba.ar>
2025-11-13 13:23:54 +0000

    Crea el 2do test test02ResultadoDeTirarDadosEstaEntre2y12

--- src/main/java/edu/fiuba/algo3/modelo/Tablero/Dados.java
+++ src/main/java/edu/fiuba/algo3/modelo/Tablero/Dados.java
@@ -1,7 +1,19 @@
 package edu.fiuba.algo3.modelo.Tablero;
 
+import java.util.Random;
+
 public class Dados {
-    public int tirar() {
-        return 6;
+    private int generarNumero() {
+        Random random = new Random();
+        return random.nextInt(6) + 1;  // 1 a 6
     }
+
+    public int tirar() {
+        int d1 = generarNumero();
+        int d2 = generarNumero();
+        int total = d1 + d2;
+
+        return total;
+    }
+
 }
--- src/main/test/edu/fiuba/algo3/UnitTest/DadosTest.java
+++ src/main/test/edu/fiuba/algo3/UnitTest/DadosTest.java
@@ -18,6 +18,20 @@ public class DadosTest {
         // Assert
         assertDoesNotThrow(() -> dados.tirar());
     }
+    @Test
+    public void test02ResultadoDeTirarDadosEstaEntre2y12() {
+
+        // Arrange
+        Dados dados = new Dados();
+
+        // Act
+        int resultado = dados.tirar();
+
+        // Assert
+        assertTrue(resultado >= 2 && resultado <= 12,
+                "El resultado debe estar entre 2 y 12, pero fue: " + resultado);
+    }
+
 
 
 }
