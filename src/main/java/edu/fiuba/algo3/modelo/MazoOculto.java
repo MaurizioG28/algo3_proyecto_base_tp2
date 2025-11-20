c8e377216fc6196b64066ed1fd63d779410cf6a7
TomasEch <techeverria@fi.uba.ar>
2025-11-20 16:25:15 -0300

    Aleatoriedad al comprar cartas

--- src/main/java/edu/fiuba/algo3/modelo/MazoOculto.java
+++ src/main/java/edu/fiuba/algo3/modelo/MazoOculto.java
@@ -1,7 +1,9 @@
 package edu.fiuba.algo3.modelo;
 
-import edu.fiuba.algo3.modelo.Cartas.CartaDesarrollo;
+import edu.fiuba.algo3.modelo.Cartas.*;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 public class MazoOculto {
@@ -18,7 +20,17 @@ public class MazoOculto {
         comprador.removerRecurso(Recurso.GRANO, 1);
         comprador.removerRecurso(Recurso.MINERAL, 1);
 
-        CartaDesarrollo cartaCaballero = new CartaDesarrollo(turno);
-        return cartaCaballero;
+        //Agarra una carta aleatoria de todas las que podemos comprar.
+
+        List<CartaDesarrollo> cartasDisponibles = new ArrayList<>();
+        cartasDisponibles.add(new CartaCaballero(turno));
+        cartasDisponibles.add(new CartaConstruccionCarreteras(turno));
+        cartasDisponibles.add(new CartaDescubrimiento(turno));
+        cartasDisponibles.add(new CartaMonopolio(turno));
+        cartasDisponibles.add(new PuntoDeVictoria(turno));
+
+        int NumeroAleatorio = azar.nextInt(cartasDisponibles.size());
+
+        return cartasDisponibles.get(NumeroAleatorio);
     }
 }
