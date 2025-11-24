d3356c1f9116cd86bc983d96c0acf065b26d003a
TomasEch <techeverria@fi.uba.ar>
2025-11-24 18:26:52 -0300

    Arreglos a los tests de CartaDesarrollo

--- src/main/java/edu/fiuba/algo3/modelo/Mocks/FakeJugador.java
+++ src/main/java/edu/fiuba/algo3/modelo/Mocks/FakeJugador.java
@@ -1,22 +1,23 @@
 package edu.fiuba.algo3.modelo.Mocks;
 
+import edu.fiuba.algo3.modelo.Color;
 import edu.fiuba.algo3.modelo.Jugador;
-import edu.fiuba.algo3.modelo.Recursos.TipoDeRecurso;
+import edu.fiuba.algo3.modelo.Recursos.*;
 
 public class FakeJugador extends Jugador {
     private boolean respuesta;
 
     public FakeJugador(boolean respuestaDefault) {
+        super("nombre1", new Color("Rojo"));
         this.respuesta = respuestaDefault;
     }
 
 
-    public boolean tiene(int madera, int ladrillo, int lana, int grano, int mineral) {
+    public boolean tiene(Madera madera, Ladrillo ladrillos, Lana lana, Mineral mineral, Grano grano) {
         return respuesta;
     }
 
-
-    public void removerRecurso(TipoDeRecurso recurso, int cantidad) {
+    public void quitarRecurso(TipoDeRecurso recurso, int cantidad) {
 
     }
 
