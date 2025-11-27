c9438592c9e9f476c221d6334557fb7fb839bea2
TomasEch <techeverria@fi.uba.ar>
2025-11-27 03:01:21 -0300

    Limpiando codigo

--- src/main/java/edu/fiuba/algo3/modelo/Cartas/CartaCaballero.java
+++ src/main/java/edu/fiuba/algo3/modelo/Cartas/CartaCaballero.java
@@ -1,13 +1,5 @@
 package edu.fiuba.algo3.modelo.Cartas;
 
-import edu.fiuba.algo3.modelo.Color;
-import edu.fiuba.algo3.modelo.Jugador;
-import edu.fiuba.algo3.modelo.Tablero.Factory.Coordenada;
-
-import java.util.List;
-import java.util.Random;
-import java.util.stream.Collectors;
-
 public class CartaCaballero extends CartaDesarrollo {
 
     public CartaCaballero(int turno) {
--- src/main/java/edu/fiuba/algo3/modelo/Cartas/CartaDesarrollo.java
+++ src/main/java/edu/fiuba/algo3/modelo/Cartas/CartaDesarrollo.java
@@ -1,7 +1,5 @@
 package edu.fiuba.algo3.modelo.Cartas;
 
-import edu.fiuba.algo3.modelo.Jugador;
-
 abstract public class CartaDesarrollo {
     private int TurnoCreacion;
 
